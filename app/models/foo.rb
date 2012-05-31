811b056c742b04057c05de66207d2ec103768f07
Andy Kim <andy@verticalbrands.com>
2012-05-31 14:07:39 -0700

    Add base rails application with foo scaffold

---
+++ app/models/foo.rb
@@ -0,0 +1,3 @@
+class Foo < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails324::Application.config.secret_token = 'f1b416348a4d27c238bccca29f481fc5f5cda71c6f18018104cef8e98dd459e37cc20c06d57bcbe4d254b650a36dcebe7f6577b06384e877a3be93b0de32c175'
